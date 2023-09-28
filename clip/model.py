a729ede8e1f3510977c9bc4d8407e8646f6d23a4
Howard <Howard@seu.com>
2023-09-28 14:53:59 +0800

    Fix the LoRA model

--- clip/model.py
+++ clip/model.py
@@ -485,6 +485,8 @@ class Adapter_CLIP(CLIP):
 class LoRA(nn.Module):
     def __init__(self, input_dim, output_dim, r):
         super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.r = r
         self.A = nn.Linear(r, output_dim)
         self.B = nn.Linear(input_dim, r)
@@ -494,29 +496,34 @@ class LoRA(nn.Module):
         nn.init.zeros_(self.B.bias)
 
     def forward(self, x):
+        x = x.reshape()
         return self.A(self.B(x.T))
 
 
-class LoRA_CLIP(nn.Module):
+class LoRA_CLIP(CLIP):
     def __init__(self, model: CLIP):
-        super().__init__()
+        super().__init__(model.embed_dim,
+                         # visual
+                         model.image_resolution,
+                         model.vision_layers,
+                         model.vision_width,
+                         model.vision_patch_size,
+                         # text
+                         model.context_length,
+                         model.vocab_size,
+                         model.transformer_width,
+                         model.transformer_heads,
+                         model.transformer_layers,
+                         )
         self.origin_model = model
-        for param in self.origin_model.parameters():
+        for param in model.parameters():
             param.requires_grad = False
-        self.LoRA = LoRA(50176, 512, np.sqrt(50176*512))
-        for param in self.LoRA.parameters():
-            param.requires_grad = True
+        self.LoRA = LoRA(224 * 224 * 3, 512, int(np.sqrt(224 * 224 * 3 * 512)))
 
     def encode_image(self, image):
-        image = self.origin_model.encode_image(image) + self.LoRA(image)
-        return image
-
-    def encode_text(self, text):
-        text = self.origin_model.encode_text(text)
-        return text
-
-    def forward(self, image, text):
-        pass
+        image_feature = self.origin_model.encode_image(image)
+        feature = self.LoRA(image)
+        return image_feature + feature
 
 
 def convert_weights(model: nn.Module):
